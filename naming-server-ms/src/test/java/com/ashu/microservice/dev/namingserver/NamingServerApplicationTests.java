feac654c3fffdc67cd23db6323e375da9d1c9b91
Ashutosh Ranjan <131255385+AshuZIP@users.noreply.github.com>
2023-05-29 09:52:44 +0530

    Naming-server-ms upload
    Puhsed the naming-server with pom.xml, source folder and the docker-file. Eureka naming server is used as the service registry.

---
+++ naming-server-ms/src/test/java/com/ashu/microservice/dev/namingserver/NamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashu.microservice.dev.namingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
